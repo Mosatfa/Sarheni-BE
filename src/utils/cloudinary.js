18f40ff35bbc47d566e342c9fab850d0e635340d
Mostafa Maged <105412165+Mosatfa@users.noreply.github.com>
2024-11-06 10:08:54 +0200

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import cloudinary from 'cloudinary';
 
 
 cloudinary.v2.config({
-    api_key:"362848569251581",
-    api_secret:"3uiLs5XZ_1JE8a3qXj2yN7PgOqo",
-    cloud_name: "dwbl7l17r",
+    api_key:"",
+    api_secret:"",
+    cloud_name: "",
     secure: true
 })
 
